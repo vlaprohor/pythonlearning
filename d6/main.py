5bc9e0d4c88780bf62aefb3c4c5d1466cf09eff0
vlaprohor <vlaprohor@gmail.com>
2026-04-12 15:34:56 +0300

    day 6: file write and read

---
+++ d6/main.py
@@ -0,0 +1,26 @@
+def save_user(name, age):
+    with open("user.txt", "w") as file:
+        file.write(f"Name: {name}\n")
+        file.write(f"Age: {age}\n")
+
+def load_user():
+    try:
+        with open("user.txt", "r") as file:
+            data = file.read()
+            print("Saved data:\n" + data)
+    except FileNotFoundError:
+        print("No saved data found")
+
+name = input("Enter your name\n")
+while True:
+    try:
+        age = int(input("Enter your age\n"))
+        if age >= 0:
+            break
+        else:
+            print("Please enter a valid age")
+    except ValueError:
+        print("Please enter a number")
+
+#save_user(name, age)
+load_user()
